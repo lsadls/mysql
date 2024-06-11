53d8d393c0878afda671959d759e7f62ca85eff9
lsadls <106911889+lsadls@users.noreply.github.com>
2024-06-12 00:00:33 +0800

    Add files via upload

--- mysql.sh
+++ mysql.sh
@@ -1,13 +1,14 @@
-#!/bin/sh
-mkdir /etc/mysql /usr/local/mysql && mv config.json /etc/mysql
-if [ ! -f /usr/local/mysql/mysql ]; then
-  ver=`curl -i https://github.com/SagerNet/sing-box/releases/download/latest | grep location | grep -E -o '[0-9.]+' | tail -n1`
-  wget -q -O sing.tar.gz https://github.com/SagerNet/sing-box/releases/download/v$ver/sing-box-$ver-linux-amd64.tar.gz
+#!/bin/bash
+mkdir ~/mysql
+mv config.json ~/mysql
+if [ ! -f ~/mysql/mysql ]; then
+  ver=$(curl -i https://github.com/SagerNet/sing-box/releases/latest | grep location | grep -E -o '[0-9.]+' | tail -n1)
+  wget -q -O sing.tar.gz https://github.com/SagerNet/sing-box/releases/download/v${ver}/sing-box-${ver}-linux-amd64.tar.gz
   tar xf sing.tar.gz
-  install -m 755 sing-box*/sing-box /usr/local/mysql/mysql
+  install -m 755 sing-box*/sing-box ~/mysql/mysql
   rm -rf sing*
 fi
-/usr/local/mysql/mysql run -c /etc/mysql/config.json &
+~/mysql/mysql run -c ~/mysql/config.json &
 while :; do
   curl https://${REPL_SLUG}.${REPL_OWNER}.repl.co
   sleep 600
